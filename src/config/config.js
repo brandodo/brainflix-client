178d094497d7c857636b796cc76824c671690035
brandodo <brandonong93@gmail.com>
2022-04-30 16:01:28 -0400

    updated config

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://brainflix-server.herokuapp.com"
+    ? "https://brainflix-server-repo.herokuapp.com"
     : process.env.REACT_APP_BACKEND_API_URL;
